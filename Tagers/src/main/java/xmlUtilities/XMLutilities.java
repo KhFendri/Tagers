891be9881dc096ea8a70a1a5863079460f981f70
MrFendri <khalil.g.fendri@gmail.com>
2021-05-29 13:29:40 +0100

    before spring boot

--- Tagers/src/main/java/xmlUtilities/XMLutilities.java
+++ Tagers/src/main/java/xmlUtilities/XMLutilities.java
@@ -9,19 +9,15 @@ import model.Ping;
 
 public class XMLutilities {
 
-	public static <Object> Document objectToXml(Object obj) {
+	public static Document objectToXml(Object obj) {
 		Document dom = null;
 
 		if (obj instanceof Ping) {
 			dom = generatePingXml((Ping) obj);
-		}
-		else if(obj instanceof Crs)
-		{
-			dom = generateLoginRequestXml((Crs)obj);
-		}
-		else if(obj instanceof String)
-		{
-			dom = generateLoginResponseXml((String)obj);
+		} else if (obj instanceof Crs) {
+			dom = generateLoginRequestXml((Crs) obj);
+		} else if (obj instanceof String) {
+			dom = generateLoginResponseXml((String) obj);
 		}
 //		else
 //		{
@@ -30,6 +26,43 @@ public class XMLutilities {
 
 		return dom;
 	}
+	
+	public static <Object> xmlToObject(Document dom)
+	{
+		Object obj = null;
+
+		// get the root and determine the type of object
+		Element root = dom.getRootElement();
+		String elementName = root.getName();
+		try
+		{
+			// extend this section to build the appropriate object
+			if(elementName.equals("login_request"))
+			{
+				// generate the LoginRequest object
+				obj = getLoginObject(root);
+			}
+			// extend this section to build the appropriate object
+			else if(elementName.equals("login_response"))
+			{
+				// generate the LoginResponse object
+				obj = getLoginResponseObject(root);
+			}
+			else if(elementName.equals("ping"))
+			{
+				// generate the LoginResponse object
+				obj = getPingObject(elementName);
+			}
+		}
+		catch(NumberFormatException ex)
+		{
+			System.out.println(ex);;
+		}
+
+		return obj;
+	}
+
+
 
 	public static Document generatePingXml(Ping p) {
 
@@ -61,19 +94,19 @@ public class XMLutilities {
 		return dom;
 
 	}
-	private static Document generateLoginRequestXml(Crs cred)
-	{
+
+	private static Document generateLoginRequestXml(Crs cred) {
 		Document dom = null;
 
 		// build the root element
 		Element root = new Element("login_request");
 		Attribute version = new Attribute("version", "1.0");
 		root.setAttribute(version);
-		
-		String mailGetter = String.format("%s", cred.getLogin() );
+
+		String mailGetter = String.format("%s", cred.getLogin());
 		Attribute pin = new Attribute("mail", mailGetter);
 		root.setAttribute(pin);
-		
+
 		String pwGetter = String.format("%s", cred.getPw());
 		Attribute account = new Attribute("password", pwGetter);
 		root.setAttribute(account);
@@ -83,25 +116,24 @@ public class XMLutilities {
 		return dom;
 
 	}
-	private static Document generateLoginResponseXml(String response)
-	{
+
+	private static Document generateLoginResponseXml(String response) {
 		Document dom = null;
 
 		// build the root element
 		Element root = new Element("login_response");
 		Attribute version = new Attribute("version", "1.0");
 		root.setAttribute(version);
-		if (String.format("%s", response )== "user not found") {
+		if (String.format("%s", response) == "user not found") {
 			Attribute resp = new Attribute("response", "user not found");
 			root.setAttribute(resp);
-		} else if (String.format("%s", response )== "invalid password") {
+		} else if (String.format("%s", response) == "invalid password") {
 			Attribute resp = new Attribute("response", "invalid password");
 			root.setAttribute(resp);
 		} else {
 			Attribute resp = new Attribute("response", response);
 			root.setAttribute(resp);
 		}
-		
 
 		dom = new Document(root);
 
@@ -109,5 +141,19 @@ public class XMLutilities {
 
 	}
 	
+	private static Object getPingObject(Element root) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object getLoginResponseObject(Element root) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object getLoginObject(Element root) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
